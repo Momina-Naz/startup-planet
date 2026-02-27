2eb9039b6b56064b8e6c7a9dbf793dc18d2e48a0
Momina-Naz <mominanaz045@gmail.com>
2026-02-27 15:18:51 +0500

    Back to initial state

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-import { app } from './app.js'
+import express from 'express'
+import { apiRouter } from './routes/apiRoutes.js'
+import cors from 'cors'
 
-const PORT = process.env.PORT || 8000
+const PORT =  8000
+
+const app = express()
+
+app.use(cors())
+
+app.use('/api', apiRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Endpoint not found. Please check the API documentation." })
+})
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
